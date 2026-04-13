60044f07108376c80c1374c97fdf58690264a037
Zenox66 <stephenreyno@gmail.com>
2026-04-13 09:46:27 +0800

    Add

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -1,3 +1,9 @@
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    openNav();
+  }
+});
+
 function openNav() {
   document.getElementById("Sidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
